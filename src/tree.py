657df42d7bf38fc0152e05a5ba9a5d9153db0a70
Bart Frenk <bart.frenk@gmail.com>
2018-03-02 11:26:16 +0100

    Initial commit

---
+++ src/tree.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+class Tree(object):
+    def __init__(self):
+        pass
+
+    def x(self, y):
+        pass
+
+
+def tree():
+    return defaultdict(tree)
